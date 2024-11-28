780c6e99ee2dc122c0332e4c62c960a9ddf9b0d6
Nikola <91883588+svili98@users.noreply.github.com>
2024-11-28 23:15:15 +0100

    Added react icons; Added more beginner project Accordion, FormValidation, HiddenSearchBar, Testimonials; Init project of Advance search, e-shop shoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import Toggler from "./Projects/BeginnerProject/ToggleBackgroundColor/Toggler";
+// import Accordion from "./Projects/BeginnerProject/Accordion/Accordion";
 
+import Nav from "./Projects/BeginnerProject/AdvanceFiltering/Navigation/Nav";
+import Products from "./Projects/BeginnerProject/AdvanceFiltering/Products/Products";
+import Recommended from "./Projects/BeginnerProject/AdvanceFiltering/Recommended/Recommended";
+import Sidebar from "./Projects/BeginnerProject/AdvanceFiltering/Sidebar/Sidebar";
+// import { accordionData } from "./Projects/BeginnerProject/Accordion/utils/content";
 const App = () => {
     return (
-        <div>
-            <Toggler />
-        </div>
+        // <div className="accordion">
+        //     {accordionData.map(({ title, content }) => (
+        //         <Accordion title={title} content={content} />
+        //     ))}
+        // </div>
+        <>
+            <Sidebar />
+            <Nav />
+            <Recommended />
+            <Products />
+        </>
     );
 };
 
---
+++ src/Projects/BeginnerProject/AdvanceFiltering/Sidebar/Colors/Colors.jsx
@@ -0,0 +1,30 @@
+import "./Colors.css";
+
+const Colors = () => {
+    return (
+        <div>
+            <label className="sidebar-label-container color-title">
+                <input type="radio" name="test3" />
+                <span className="checkmark"></span>All
+            </label>
+            <label className="sidebar-label-container">
+                <input type="radio" name="test3" />
+                <span className="checkmark"></span>Black
+            </label>
+            <label className="sidebar-label-container">
+                <input type="radio" name="test3" />
+                <span className="checkmark"></span>Blue
+            </label>
+            <label className="sidebar-label-container">
+                <input type="radio" name="test3" />
+                <span className="checkmark"></span>Red
+            </label>
+            <label className="sidebar-label-container">
+                <input type="radio" name="test3" />
+                <span className="checkmark"></span>White
+            </label>
+        </div>
+    );
+};
+
+export default Colors;
